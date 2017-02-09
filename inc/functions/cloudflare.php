6c1a3a5957083773c3d804e68612d3538e39a13d
WpPremiumThemes <wp.premium.themes.mirror@gmail.com>
2017-02-10 00:51:56 +0200

    v2.8.20

--- inc/functions/cloudflare.php
+++ inc/functions/cloudflare.php
@@ -2,121 +2,220 @@
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 /**
- * Get a WP_Rocket_CloudFlareAPI instance
+ * Get a CloudFlare\Api instance & the zone_id corresponding to the domain
  *
+ * @since 2.8.18 Add try/catch to prevent fatal error Uncaugh Exception
+ * @since 2.8.16 Update to CloudFlare API v4
  * @since 2.5
  *
- * @return obj WP_Rocket_CloudFlareAPI instance
+ * @return mixed bool|object CloudFlare instance & zone_id if credentials are correct, false otherwise
  */
 function get_rocket_cloudflare_instance() {
 	$cf_email   = get_rocket_option( 'cloudflare_email', null );
 	$cf_api_key = ( defined( 'WP_ROCKET_CF_API_KEY' ) ) ? WP_ROCKET_CF_API_KEY : get_rocket_option( 'cloudflare_api_key', null );
 
-	if( isset( $cf_email, $cf_api_key ) ) {
-		return WP_Rocket_CloudFlareAPI::instance( $cf_email, $cf_api_key );
+	if ( isset( $cf_email, $cf_api_key ) ) {
+        	$cf_instance = ( object ) [ 'auth' => new Cloudflare\Api( $cf_email, $cf_api_key ) ];
+
+        	try {
+                $zone_instance = new CloudFlare\Zone( $cf_instance->auth );
+				$cf_domain     = get_rocket_option( 'cloudflare_domain', null );
+				$zone          = $zone_instance->zones( $cf_domain );
+
+                if ( isset( $zone->result[0]->id ) ) {
+                    $cf_instance->zone_id = $zone->result[0]->id;
+                    return $cf_instance;
+                }
+            } catch ( Exception $e ) {}
+
+            return false;
 	}
+
 	return false;
 }
 
 /**
- * Returns the main instance of WP_Rocket_CloudFlareAPI to prevent the need to use globals.
+ * Returns the main instance of CloudFlare API to prevent the need to use globals.
  */
 $GLOBALS['rocket_cloudflare'] = get_rocket_cloudflare_instance();
 
 /**
  * Get all the current CloudFlare settings for a given domain.
  *
+ * @since 2.8.16 Update to CloudFlare API v4
  * @since 2.5
  *
- * @return void
+ * @return Array
  */
 function get_rocket_cloudflare_settings() {
-	if( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
+	if ( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
 		return false;
 	}
 	
-	$domain 	 = get_rocket_option( 'cloudflare_domain' );
-	$cf_settings = (array) $GLOBALS['rocket_cloudflare']->zone_settings( $domain )->response->result->objs;
-	return reset(( $cf_settings ));
-}
-
-/**
- * Set the CloudFlare Development Mode.
- *
- * @since 2.5
- *
- * @return void
- */
-function set_rocket_cloudflare_cache_lvl( $mode ) {
-	if( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
+	try {
+		$cf_settings_instance = new CloudFlare\Zone\Settings( $GLOBALS['rocket_cloudflare']->auth );
+		$cf_settings          = $cf_settings_instance->settings( $GLOBALS['rocket_cloudflare']->zone_id );
+		$cf_minify            = $cf_settings->result[16]->value;
+		$cf_minify_value      = 'on';
+	
+		if ( $cf_minify->js === 'off' || $cf_minify->css === 'off' || $cf_minify->html === 'off' ) {
+	    	$cf_minify_value = 'off';
+		}
+	
+		$cf_settings_array  = array(
+	    	'cache_level'       => $cf_settings->result[5]->value,
+	    	'minify'            => $cf_minify_value,
+	    	'rocket_loader'     => $cf_settings->result[25]->value,
+	    	'browser_cache_ttl' => $cf_settings->result[3]->value
+		);
+	
+		return $cf_settings_array;
+	} catch ( Exception $e ) {
 		return false;
 	}
-	
-	$domain = get_rocket_option( 'cloudflare_domain' );
-	$GLOBALS['rocket_cloudflare']->cache_lvl( $domain, $mode );
 }
 
-
 /**
- * Set the CloudFlare Caching Level.
+ * Set the CloudFlare Development mode.
  *
+ * @since 2.8.16 Update to CloudFlare API v4
  * @since 2.5
  *
  * @return void
  */
 function set_rocket_cloudflare_devmode( $mode ) {
-	if( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
+	if ( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
 		return false;
 	}
-	
-	$domain = get_rocket_option( 'cloudflare_domain' );
-	$GLOBALS['rocket_cloudflare']->devmode( $domain, $mode );
+
+    if ( ( int ) $mode === 0 ) {
+        $value = 'off';
+    } else if ( ( int ) $mode === 1 ) {
+        $value = 'on';
+    }
+
+	try {
+		$cf_settings = new CloudFlare\Zone\Settings( $GLOBALS['rocket_cloudflare']->auth );
+		$cf_settings->change_development_mode( $GLOBALS['rocket_cloudflare']->zone_id, $value );
+	} catch ( Exception $e ) {}
 }
 
 /**
- * Set the CloudFlare Rocket Loader.
+ * Set the CloudFlare Caching level.
  *
+ * @since 2.8.16 Update to CloudFlare API v4
  * @since 2.5
  *
  * @return void
  */
-function set_rocket_cloudflare_async( $mode ) {
-	if( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
+function set_rocket_cloudflare_cache_level( $mode ) {
+	if ( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
 		return false;
 	}
 	
-	$domain = get_rocket_option( 'cloudflare_domain' );
-	$GLOBALS['rocket_cloudflare']->async( $domain, $mode );
+	try {
+		$cf_settings = new CloudFlare\Zone\Settings( $GLOBALS['rocket_cloudflare']->auth );
+		$cf_settings->change_cache_level( $GLOBALS['rocket_cloudflare']->zone_id, $mode );
+	} catch ( Exception $e ) {}
 }
 
 /**
  * Set the CloudFlare Minification.
  *
+ * @since 2.8.16 Update to CloudFlare API v4
  * @since 2.5
  *
  * @return void
  */
 function set_rocket_cloudflare_minify( $mode ) {
-	if( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
+	if ( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
+		return false;
+	}
+
+    $cf_minify_settings = array(
+        'css'  => $mode,
+        'html' => $mode,
+        'js'   => $mode
+    );
+	
+	try {
+		$cf_settings = new CloudFlare\Zone\Settings( $GLOBALS['rocket_cloudflare']->auth );
+		$cf_settings->change_minify( $GLOBALS['rocket_cloudflare']->zone_id, $cf_minify_settings );
+	} catch ( Exception $e ) {}
+}
+
+/**
+ * Set the CloudFlare Rocket Loader.
+ *
+ * @since 2.8.16 Update to CloudFlare API v4
+ * @since 2.5
+ *
+ * @return void
+ */
+function set_rocket_cloudflare_rocket_loader( $mode ) {
+	if ( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
 		return false;
 	}
 	
-	$domain = get_rocket_option( 'cloudflare_domain' );
-	$GLOBALS['rocket_cloudflare']->minify( $domain, $mode );
+	try {
+		$cf_settings = new CloudFlare\Zone\Settings( $GLOBALS['rocket_cloudflare']->auth );
+		$cf_settings->change_rocket_loader( $GLOBALS['rocket_cloudflare']->zone_id, $mode );
+	} catch ( Exception $e ) {}
+}
+
+/**
+ * Set the Browser Cache TTL in CloudFlare.
+ *
+ * @since 2.8.16
+ *
+ * @return void
+ */
+function set_rocket_cloudflare_browser_cache_ttl( $mode ) {
+	if ( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
+		return false;
+	}
+	
+	try {
+		$cf_settings = new CloudFlare\Zone\Settings( $GLOBALS['rocket_cloudflare']->auth );
+		$cf_settings->change_browser_cache_ttl( $GLOBALS['rocket_cloudflare']->zone_id, $mode );
+	} catch ( Exception $e ) {}
 }
 
 /**
  * Purge CloudFlare cache.
  *
+ * @since 2.8.16 Update to CloudFlare API v4
  * @since 2.5
  *
  * @return void
  */
 function rocket_purge_cloudflare() {
-	if( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
+	if ( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
 		return false;
 	}
 	
-	$domain = get_rocket_option( 'cloudflare_domain' );
-	$GLOBALS['rocket_cloudflare']->fpurge_ts( $domain );
+	try {
+		$cf_cache = new CloudFlare\Zone\Cache( $GLOBALS['rocket_cloudflare']->auth );
+		$cf_cache->purge( $GLOBALS['rocket_cloudflare']->zone_id, true );
+	} catch ( Exception $e ) {}
+}
+
+/**
+ * Get CloudFlare IPs.
+ *
+ * @since 2.8.16
+ *
+ * @return Object Result of API request
+ */
+function rocket_get_cloudflare_ips() {
+    if( ! is_object( $GLOBALS['rocket_cloudflare'] ) ) {
+		return false;
+	}
+
+    try {
+        $cf_ips_instance = new CloudFlare\IPs( $GLOBALS['rocket_cloudflare']->auth );
+        return $cf_ips_instance->ips();
+    } catch ( Exception $e ) {
+        return false;
+    }
 }
